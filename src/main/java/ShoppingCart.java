79a50f9391e8787156c38d4f9dc171e0280db5da
SaileshK1103 <sailesh.karki@metropolia.fi>
2026-03-21 14:29:49 +0200

    Added loaclization option for japanese language.

--- src/main/java/ShoppingCart.java
+++ src/main/java/ShoppingCart.java
@@ -17,12 +17,14 @@ public class ShoppingCart {
         System.out.println("1. English");
         System.out.println("2. Finnish(Suomi)");
         System.out.println("3. Swedish (Svenska)");
+        System.out.println("4. Japanese (日本語)");
 
         int choice = scanner.hasNextInt() ? scanner.nextInt() : 1;
 
         Locale locale = switch (choice) {
             case 2 -> new Locale("fi", "FI");
             case 3 -> new Locale("sv", "SE");
+            case 4 -> new Locale("ja", "JP");
             default -> new Locale("en", "US");
         };
 
